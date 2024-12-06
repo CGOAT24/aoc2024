b0fabf475f55441e0c09822afcbb1c774ac2989b
Chad Gauthier <chad.gauthier32@gmail.com>
2024-12-06 11:30:11 -0500

    part 2 wip

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,13 +1,13 @@
 use std::collections::{HashSet, LinkedList};
-use std::ptr::replace;
 
+#[derive(Clone, Eq, Hash, PartialEq)]
 enum CellType {
     Empty,
     Obstacle
 }
 
 impl CellType {
-    pub(crate) fn clone(&self) -> CellType {
+    fn clone(&self) -> CellType {
         match self {
             CellType::Empty => CellType::Empty,
             CellType::Obstacle => CellType::Obstacle
@@ -15,40 +15,101 @@ impl CellType {
     }
 }
 
+#[derive(Eq, Hash, PartialEq, Clone)]
 struct Cell {
     cell_type: CellType,
     position: (usize, usize)
 }
 
+#[derive(Eq, Hash, PartialEq, Clone)]
+struct Visit {
+    position: (usize, usize),
+    direction: Direction
+}
+
 struct Map {
     cells: Vec<Vec<Cell>>,
-    player_position: (usize, usize),
-    visited: HashSet<(usize, usize)>
+    guard_pos: (usize, usize),
+    visited: HashSet<Visit>,
+    current_dir: Direction
 }
 
 impl Map {
-    fn at(&self, pos: (usize, usize)) -> Option<CellType> {
-        self.cells.get(pos.0).and_then(|row| row.get(pos.1)).map(|cell| cell.cell_type.clone())
+    fn at(&self, pos: (usize, usize)) -> Option<Cell> {
+        if pos.0 >= self.cells.len() || pos.1 >= self.cells[0].len() {
+            return None;
+        }
+        Some(self.cells[pos.0][pos.1].clone())
     }
 
-    fn print(&self, obstacle: (usize, usize)) {
+    fn get_next_position(&self) -> Cell {
+        match self.current_dir{
+            Direction::Up => self.at((self.guard_pos.0 - 1, self.guard_pos.1)).unwrap(),
+            Direction::Down => self.at((self.guard_pos.0 + 1, self.guard_pos.1)).unwrap(),
+            Direction::Left => self.at((self.guard_pos.0, self.guard_pos.1 - 1)).unwrap(),
+            Direction::Right => self.at((self.guard_pos.0, self.guard_pos.1 + 1)).unwrap()
+        }
+    }
+
+    fn is_at_edge(&self) -> bool {
+        match self.current_dir {
+            Direction::Up => self.guard_pos.0 == 0,
+            Direction::Down => self.guard_pos.0 == self.cells.len() - 1,
+            Direction::Left => self.guard_pos.1 == 0,
+            Direction::Right => self.guard_pos.1 == self.cells[0].len() - 1
+        }
+    }
+
+    fn change_direction(&mut self) {
+        self.current_dir = match self.current_dir {
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down
+        };
+    }
+
+    fn get_unique_visited(&self) -> HashSet<(usize, usize)> {
+        self.visited.iter().map(|visit| visit.position).collect()
+    }
+
+    fn print(&self) {
         for i in 0..self.cells.len() {
             for j in 0..self.cells[i].len() {
-                if self.cells[i][j].position == self.player_position {
+                if self.cells[i][j].position == self.guard_pos {
                     print!("^");
                     continue;
                 }
-
-                if self.visited.contains(&self.cells[i][j].position) {
+                if self.visited.iter().any(|visit| visit.position == (i, j)) {
                     print!("X");
                     continue;
                 }
 
+                match self.cells[i][j].cell_type {
+                    CellType::Empty => print!("."),
+                    CellType::Obstacle => print!("#")
+                }
+            }
+            println!();
+        }
+        println!();
+    }
+
+    fn print_obstacle(&self, obstacle: (usize, usize)) {
+        for i in 0..self.cells.len() {
+            for j in 0..self.cells[i].len() {
+                if self.cells[i][j].position == self.guard_pos {
+                    print!("^");
+                    continue;
+                }
+                if self.visited.contains(&Visit { position: (i, j), direction: self.current_dir.clone() }) {
+                    print!("X");
+                    continue;
+                }
                 if self.cells[i][j].position == obstacle {
                     print!("O");
                     continue;
                 }
-
                 match self.cells[i][j].cell_type {
                     CellType::Empty => print!("."),
                     CellType::Obstacle => print!("#")
@@ -64,12 +125,13 @@ impl Clone for Map {
     fn clone(&self) -> Self {
         Map {
             cells: self.cells.iter().map(|row| row.iter().map(|cell| Cell { cell_type: cell.cell_type.clone(), position: cell.position }).collect()).collect(),
-            player_position: self.player_position,
-            visited: self.visited.clone()
+            guard_pos: self.guard_pos,
+            visited: self.visited.clone(),
+            current_dir: self.current_dir.clone(),
         }
     }
 }
-
+#[derive(Eq, Hash, PartialEq, Clone)]
 enum Direction {
     Up,
     Down,
@@ -77,103 +139,83 @@ enum Direction {
     Right
 }
 
-fn part1(map: &mut Map) -> HashSet<(usize, usize)> {
-    let mut current_dir = Direction::Up;
-
+fn part1(map: &mut Map) {
     loop {
-        map.visited.insert(map.player_position);
+        map.visited.insert(Visit { position: map.guard_pos, direction: map.current_dir.clone() });
 
-        if is_at_edge(map.player_position, (map.cells.len(), map.cells[0].len()), &current_dir) {
+        if map.is_at_edge() {
             break;
         }
 
-        let mut next_position = get_next_position(&map, &current_dir);
-        if matches!(map.at(next_position).unwrap(), CellType::Obstacle) {
-            current_dir = match current_dir {
-                Direction::Up => Direction::Right,
-                Direction::Down => Direction::Left,
-                Direction::Left => Direction::Up,
-                Direction::Right => Direction::Down
-            };
-            next_position = get_next_position(&map, &current_dir);
+        let mut next_position = map.get_next_position();
+        if matches!(next_position.cell_type, CellType::Obstacle) {
+            map.change_direction();
+            next_position = map.get_next_position();
         }
 
-        map.player_position = next_position;
+        map.guard_pos = next_position.position;
     }
-    map.visited.clone()
 }
 
-fn part2(map: &mut Map, og_visited: &HashSet<(usize, usize)>) -> usize {
-    let mut potential = LinkedList::from_iter(og_visited.iter());
-    let mut valid_count = 0;
-    let starting_pos = map.player_position;
+fn part2(map: &mut Map, visited: HashSet<Visit>) -> usize {
+    let mut potential = LinkedList::from_iter(visited
+        .iter()
+        .map(|visit| visit.position)
+        .collect::<HashSet<(usize, usize)>>()
+    );
+    let mut valid_obstacles = HashSet::new();
+    let starting_pos = map.guard_pos;
 
     while potential.len() > 0 {
         let elem = potential.pop_back().unwrap();
-        let mut current_dir = Direction::Up;
-        let mut visited: HashSet<(usize, usize)> = HashSet::new();
-        map.player_position = starting_pos;
+        let mut current_map = map.clone();
+        current_map.cells[elem.0][elem.1].cell_type = CellType::Obstacle;
+        map.guard_pos = starting_pos;
 
-        loop {
-            map.print(*elem);
-            if visited.contains(&map.player_position) {
-                break;
-            }
-            visited.insert(map.player_position);
-
-            if is_at_edge(map.player_position, (map.cells.len(), map.cells[0].len()), &current_dir) {
-                break;
-            }
-
-            let mut next_position = get_next_position(&map, &current_dir);
-            if matches!(map.at(next_position).unwrap(), CellType::Obstacle) || next_position == *elem {
-                current_dir = match current_dir {
-                    Direction::Up => Direction::Right,
-                    Direction::Down => Direction::Left,
-                    Direction::Left => Direction::Up,
-                    Direction::Right => Direction::Down
-                };
-                next_position = get_next_position(&map, &current_dir);
-            }
-
-            map.player_position = next_position;
+        if is_a_cycle(current_map) {
+            println!("Cycle at {:?}", elem);
+            valid_obstacles.insert(elem);
         }
-        if visited.len() > og_visited.len() {
-            valid_count += 1;
+        else {
+            println!("Not a cycle at {:?}", elem);
         }
     }
-    valid_count
+    valid_obstacles.len()
 }
 
-fn is_at_edge(current_position: (usize, usize), map_size: (usize, usize), direction: &Direction) -> bool {
-    match direction {
-        Direction::Up => current_position.0 == 0,
-        Direction::Down => current_position.0 == map_size.0 - 1,
-        Direction::Left => current_position.1 == 0,
-        Direction::Right => current_position.1 == map_size.1 - 1
-    }
-}
+fn is_a_cycle(mut map: Map) -> bool {
+    loop {
+        let is_not_cycle = map.visited.insert(Visit { position: map.guard_pos, direction: map.current_dir.clone() });
 
-fn get_next_position(map: &Map, direction: &Direction) -> (usize, usize) {
-    match direction {
-        Direction::Up => (map.player_position.0 - 1, map.player_position.1),
-        Direction::Down => (map.player_position.0 + 1, map.player_position.1),
-        Direction::Left => (map.player_position.0, map.player_position.1 - 1),
-        Direction::Right => (map.player_position.0, map.player_position.1 + 1)
+        if !is_not_cycle {
+            return true;
+        }
+
+        if map.is_at_edge() {
+            break;
+        }
+
+        let mut next_position = map.get_next_position();
+        if matches!(next_position.cell_type, CellType::Obstacle) {
+            map.change_direction();
+            next_position = map.get_next_position();
+        }
+
+        map.guard_pos = next_position.position;
     }
+    false
 }
 
 fn parse_map(lines: &Vec<String>) -> Map {
     let mut cells: Vec<Vec<Cell>> = Vec::new();
-    let mut player_position = (0, 0);
+    let mut guard_pos = (0, 0);
     for i in 0..lines.len() {
         let mut row = Vec::new();
         for j in 0..lines[i].chars().count() {
-
             match lines[i].chars().nth(j).unwrap() {
                 '#' => row.push(Cell { cell_type: CellType::Obstacle, position: (i, j) }),
                 '^' =>  {
-                    player_position = (i, j);
+                    guard_pos = (i, j);
                     row.push(Cell { cell_type: CellType::Empty, position: (i, j) })
                 },
                 _ => row.push(Cell { cell_type: CellType::Empty, position: (i, j) })
@@ -183,20 +225,21 @@ fn parse_map(lines: &Vec<String>) -> Map {
     }
     Map {
         cells,
-        player_position,
-        visited: HashSet::new()
+        guard_pos,
+        visited: HashSet::new(),
+        current_dir: Direction::Up
     }
 }
 
 fn main() {
-    let lines = utils::read_file("input_test.txt".as_ref()).unwrap();
-
+    let lines = utils::read_file("input.txt".as_ref()).unwrap();
     let mut map = parse_map(&lines);
 
-    let visited = part1(&mut map.clone());
+    let mut map1 = map.clone();
+    part1(&mut map1);
 
-    let result2 = part2(&mut map, &visited);
+    let result2 = part2(&mut map, map1.visited.clone());
 
-    println!("Part 1: {}", visited.len());
+    println!("Part 1: {}", map1.get_unique_visited().len());
     println!("Part 2: {}", result2);
 }
